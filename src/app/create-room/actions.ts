e83073d44b0a01f7a14b3d2cb21fa7b5bc846e41
Palmis <melvin_02@hotmail.se>
2024-04-30 01:05:25 +0200

    switching to jwt strategy, adding id to the session, creating the room when submitting the form

---
+++ src/app/create-room/actions.ts
@@ -0,0 +1,15 @@
+'use server';
+
+import { getSession } from "@/auth";
+import { db } from "@/db";
+import { Room, room } from "@/db/schema";
+
+export async function createRoomAction(roomData: Omit<Room, "id" | "userId">) {
+    const session = await getSession();
+
+    if (!session) {
+        throw new Error("you must be logged in to create room")
+    }
+
+    await db.insert(room).values({ ...roomData, userId: session?.user.id });
+}
